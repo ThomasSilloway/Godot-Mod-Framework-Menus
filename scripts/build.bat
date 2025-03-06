7997801d6da164681eb274c2b8c4fbab546a3815
Thomas Silloway <tcsillow@gmail.com>
2025-03-05 22:43:17 -0500

    Update config files to support these mods

--- scripts/build.bat
+++ scripts/build.bat
@@ -45,21 +45,21 @@ echo Export path: %export_path%
 
 set export_path_mod=mods\
 set mod_filetype=.zip
-@REM Example for configuring mods
-@REM set mod_name_00=main_menu
+set mod_name_00=main_menu
+set mod_name_01=settings_menu
 
 :: Create directories
 mkdir "%export_path%"
 mkdir "%export_path%%export_path_mod%"
+
 
 :: Copy mod order template
 copy "%scripts_path%mod_order_template.yaml" "%export_path%%export_path_mod%mod_order.yaml"
 
 :: Export projects
 %godot_path% --headless --path %project_path%%startup_project_path% --export-release "Windows Desktop" %export_path%%export_path_file%
-
-@REM Example for building mods
-@REM %godot_path% --headless --path %project_path%%mod_name_00% --export-pack "Windows Desktop" %export_path%%export_path_mod%%mod_name_00%%mod_filetype%
+%godot_path% --headless --path %project_path%%mod_name_00% --export-pack "Windows Desktop" %export_path%%export_path_mod%%mod_name_00%%mod_filetype%
+%godot_path% --headless --path %project_path%%mod_name_01% --export-pack "Windows Desktop" %export_path%%export_path_mod%%mod_name_01%%mod_filetype%
 
 :: Check if the main executable was created successfully
 if exist "%export_path%%run_path_file%" (
